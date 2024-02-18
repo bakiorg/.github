1e8070a72d32c2bf28a984f380c2f027fbb5409f
Blagoja Stamatovski <bazestamatovski@googlemail.com>
2024-02-18 11:32:48 +0100

    Update workflow names

--- modules/team/main.tf
+++ modules/team/main.tf
@@ -1,21 +1,21 @@
 variable "team_name" {
-  type = string
+  type        = string
   description = "The name of the team. Must be unique within the same organization"
 }
 
 variable "member_usernames" {
-  type = set(string)
+  type        = set(string)
   description = "The set of usernames that are members of this team."
-  default = []
+  default     = []
 }
 
 resource "github_team" "team" {
-  name = var.team_name
+  name    = var.team_name
   privacy = "closed"
 }
 
 resource "github_team_membership" "some_team_membership" {
-  team_id  = github_team.team.id
+  team_id = github_team.team.id
 
   for_each = var.member_usernames
   username = each.value
@@ -23,11 +23,11 @@ resource "github_team_membership" "some_team_membership" {
 }
 
 resource "github_team_settings" "code_review_settings" {
-  team_id    = github_team.team.id
+  team_id = github_team.team.id
   review_request_delegation {
-    algorithm = "LOAD_BALANCE"
+    algorithm    = "LOAD_BALANCE"
     member_count = 1
-    notify = true
+    notify       = true
   }
 }
 
