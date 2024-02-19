c61e352c41f694069a32320102dab1c3bb051b3b
Blagoja Stamatovski <bazestamatovski@googlemail.com>
2024-02-19 09:48:57 +0100

    New workflow improvements

--- organization_members.tf
+++ organization_members.tf
@@ -1,7 +1,7 @@
-#resource "github_membership" "call_me_baki" {
-#  username = "call-me-baki"
-#  role     = "admin"
-#}
+resource "github_membership" "call_me_baki" {
+  username = "call-me-baki"
+  role     = "member"
+}
 
 resource "github_membership" "bakilol" {
   username = "bakilol"
