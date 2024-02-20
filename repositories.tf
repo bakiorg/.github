90d2208e394b4cc84e5bbe225760a5c7f6ff22e0
Blagoja Stamatovski <bazestamatovski@googlemail.com>
2024-02-20 07:20:36 +0100

    Add repository variables

--- repositories.tf
+++ repositories.tf
@@ -14,9 +14,24 @@ module "github" {
     }
   }
   actions_variables_global = {
+    bakiorg_github_app_name = {
+      variable_key   = "BAKIORG_GITHUB_APP_NAME"
+      variable_value = data.github_app.bakiorg_github.id
+    }
+
+    bakiorg_github_app_bot_name = {
+      variable_key   = "BAKIORG_GITHUB_APP_BOT_NAME"
+      variable_value = local.bakiorg_github_app_bot_name
+    }
+
     bakiorg_github_app_id = {
       variable_key   = "BAKIORG_GITHUB_APP_ID"
       variable_value = data.github_app.bakiorg_github.id
+    }
+
+    bakiorg_github_app_bot_id = {
+      variable_key   = "BAKIORG_GITHUB_APP_BOT_ID"
+      variable_value = local.bakiorg_github_app_bot_id
     }
 
     bakiorg_github_app_installation_id = {
